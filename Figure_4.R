08e08f59133a6b71e6e05bd1b3902bb971bdd268
Guillermo L. Florez Montero <gflorezmontero@gmail.com>
2020-10-20 23:29:47 -0300

    Updated figure 4

--- Figure_4.R
+++ Figure_4.R
@@ -1,7 +1,8 @@
 ################################################################################
 #
-# FIGURE 4. Number of recorded interactions by plant life form and interaction type
-#
+# FIGURE 4. 
+# A. Number of recorded interactions by plant life form and interaction type
+# B. 
 ################################################################################
 
 
@@ -25,12 +26,7 @@ rm(list= ls())
 library(dplyr)
 library(ggplot2)
 library(tidyr)
-library(extrafont)
-library(ggpubr)
-library(grid)
-library(ggpirate)
-library(yarrr)
-
+library(cowplot)
 
 ################################################################################
 ##### IMPORT THE DATA
@@ -40,61 +36,161 @@ library(yarrr)
 # Import the data set with the interaction records and filter out "unidentified"
 # plant genera
 records <- read.csv("./data/NeoBat_Interactions_records.csv")
-records <- records %>% 
-      filter(!is.na(LifeForm))
 
 # Check the data
 class(records)
 str(records)
 head(records)
 
+
 # Make a new table in which each observation represents a plant species,
 # and the number of interactions is given by type (nectarivory and frugivory)
 # and plant life form.
-data <- records %>% 
-      group_by(CurrentPlantSpecies,CurrentBatSpecies, 
-               LifeForm, Interaction) %>%
-      summarise() %>%
-      group_by(CurrentPlantSpecies, LifeForm, Interaction) %>%
-      summarise(N_interactions = n())
+lifeform <- records %>%
+   dplyr::filter(!is.na(LifeForm)) %>%
+   dplyr::group_by(CurrentPlantSpecies,CurrentBatSpecies, 
+                   LifeForm, Interaction) %>%
+   dplyr::summarise() %>%
+   dplyr::group_by(CurrentPlantSpecies, LifeForm, Interaction) %>%
+   dplyr::summarise(N_interactions = n())
 
 #Check the data
-class(data)
-str(data)
-head(data)
+class(lifeform)
+str(lifeform)
+head(lifeform)
+
+
+# Make another table in which each observation represents a plant species,
+# and the number of interactions is given by type (nectarivory and frugivory)
+# and plant successional stage.
+
+
+sstage <- records %>%
+   dplyr::filter(!is.na(SuccessionalStage)) %>%
+   dplyr::group_by(CurrentPlantSpecies,CurrentBatSpecies, 
+                   SuccessionalStage, Interaction) %>%
+   dplyr::summarise() %>%
+   dplyr::group_by(CurrentPlantSpecies, SuccessionalStage, Interaction) %>%
+   dplyr::summarise(N_interactions = n())
+
+#Check the data
+class(sstage)
+str(sstage)
+head(sstage)
+
+
+# Make a third table in which each observation represents a bat species,
+# and the number of interactions is given by type (nectarivory and frugivory)
+# and bat trophic guild.
+
+
+batguild <- records %>%
+   dplyr::filter(!is.na(TrophicGuild)) %>%
+   dplyr::group_by(CurrentPlantSpecies,CurrentBatSpecies, 
+                   TrophicGuild, Interaction) %>%
+   dplyr::summarise() %>%
+   dplyr::group_by(CurrentBatSpecies, TrophicGuild, Interaction) %>%
+   dplyr::summarise(N_interactions = n())
+
+#Check the data
+class(batguild)
+str(batguild)
+head(batguild)
 
 
 # Make a violin plot with jitter
+# number of interactions for each type of life form of plants
 
-colors = c("green", "purple")
-
-g1 <- ggplot(data = data, aes(x = Interaction, y = N_interactions, fill = Interaction)) + 
-   geom_violin(alpha = 0.1, aes(colour = Interaction,
-                                             fill = Interaction)) + 
-   facet_wrap(. ~ LifeForm) +
-   geom_jitter(alpha = 0.5, aes(colour = Interaction,
-                                              fill = Interaction)) +
-   theme_bw() + 
-   labs(x = " ", y = "Number of interactions (bat species)") +
-   theme(axis.text.y = element_text(size = 9, colour = "black",
-                                    family = "Arial Narrow"),
-         axis.text.x = element_text(size = 9, colour = "black",
-                                    family = "Arial Narrow"),
+p1 <- ggplot(data = lifeform, aes(x = Interaction, y = N_interactions)) + 
+   geom_violin(alpha = 0.3,colour = "black",
+               fill = "#980063") + 
+   facet_wrap(. ~ LifeForm, nrow = 3) +
+   geom_jitter(alpha = 0.5, colour = "#980063",
+               fill = "#980063") +
+   theme_bw() +
+   ylim(0,24) +
+   labs(x = " ", y = "Number of interactions", 
+        title = "A. Number of interactions of plant species by life form") +
+   theme(panel.grid = element_blank(),
+         title = element_text(size = 10, colour = "black", vjust = 3,
+                              face = "bold"),
+         axis.text.y = element_text(size = 9, colour = "black"),
+         axis.text.x = element_text(size = 9, colour = "black"),
          axis.title.y = element_text(size = 10, colour = "black", vjust = 3,
-                                     family = "Arial Narrow", face = "bold"),
+                                     face = "bold"),
          #the text of the grey panel
          strip.text = element_text(size = 10, colour = "black",
-                                   family = "Arial Narrow", face = "bold"),
-         plot.margin = unit(c(1,1,2,1), "lines"))
+                                   face = "bold"),
+         plot.margin = unit(c(1,1,0.4,1), "lines"))
 
 # See the plot
-g1
+p1
+
+
+# Make a violin plot with jitter
+# number of interactions for each type of successional stage of plants
+
+p2 <- ggplot(data = sstage, aes(x = Interaction, y = N_interactions)) + 
+   geom_violin(alpha = 0.3,colour = "black",
+               fill = "#980063") + 
+   facet_wrap(. ~ SuccessionalStage) +
+   geom_jitter(alpha = 0.5, colour = "#980063",
+               fill = "#980063") +
+   theme_bw() + 
+   ylim(0,24) +
+   labs(x = " ", y = "Number of interactions", 
+        title = "B. Number of interactions of plant species by succesional stage") +
+   theme(panel.grid = element_blank(),
+         title = element_text(size = 10, colour = "black", vjust = 3,
+                              face = "bold"),
+         axis.text.y = element_text(size = 9, colour = "black"),
+         axis.text.x = element_text(size = 9, colour = "black"),
+         axis.title.y = element_text(size = 10, colour = "black", vjust = 3,
+                                     face = "bold"),
+         #the text of the grey panel
+         strip.text = element_text(size = 10, colour = "black",
+                                   face = "bold"),
+         plot.margin = unit(c(0.4,1,0.5,1), "lines"))
+
+# See the plot
+p2
+
+
+# Make a violin plot with jitter
+# number of interactions for each type of trophic guild of bats
+
+p3 <- ggplot(data = batguild, aes(x = Interaction, y = N_interactions)) + 
+   geom_violin(alpha = 0.3,colour = "black",
+               fill = "#C59F00") + 
+   facet_wrap(. ~ TrophicGuild) +
+   geom_jitter(alpha = 0.5, colour = "#C59F00",
+               fill = "#C59F00") +
+   theme_bw() + 
+   labs(x = " ", y = "Number of interactions", 
+        title = "C. Number of interactions of bat species by trophic guild") +
+   theme(panel.grid = element_blank(),
+         title = element_text(size = 10, colour = "black", vjust = 3,
+                              face = "bold"),
+         axis.text.y = element_text(size = 9, colour = "black"),
+         axis.text.x = element_text(size = 9, colour = "black"),
+         axis.title.y = element_text(size = 10, colour = "black", vjust = 3,
+                                     face = "bold"),
+         #the text of the grey panel
+         strip.text = element_text(size = 10, colour = "black",
+                                   face = "bold"),
+         plot.margin = unit(c(0.4,1,1,1), "lines"))
+
+# See the plot
+p3
+
 
 
 # Export the map as a PNG image
 png("./Figures/Figure_4.png", res = 300,
-    width = 3000, height = 2000, unit = "px")
-g1
+    width = 2000, height = 3200, unit = "px")
+
+cowplot::plot_grid(p1, p2, p3, nrow = 3, 
+                   rel_heights = c(2.6,1,1.8))
 
 dev.off()
 
